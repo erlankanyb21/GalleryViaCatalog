7dd295ca83d8f968bdfe6233e5abefdbbcc19d75
Erlan <kanybekov668@gmail.com>
2023-06-04 10:18:20 +0600

    надеюсь

--- app/src/main/java/com/example/galleryviacatalog/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/galleryviacatalog/ui/home/HomeFragment.kt
@@ -1,15 +1,27 @@
 package com.example.galleryviacatalog.ui.home
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.core.view.isVisible
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.galleryviacatalog.R
 import com.example.galleryviacatalog.databinding.FragmentHomeBinding
 import com.example.galleryviacatalog.ui.home.base.BaseFragment
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.fragment_home) {
 
@@ -17,6 +29,11 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
     override val viewModel by viewModels<HomeViewModel>()
 
     private lateinit var galleryPhotoAdapter: GalleryPhotoAdapter
+
+    private companion object {
+        const val REQUEST_IMAGE_GALLERY = 1
+        const val REQUEST_READ_EXTERNAL_STORAGE = 2
+    }
 
     override fun initialize() {
         viewModel.getStats()
@@ -38,20 +55,20 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
 
     private fun onItemLongClick(selectedItems: List<Int>) {
         binding.deleteItem.setOnClickListener {
-            val alertDialog = AlertDialog.Builder(requireContext())
+            AlertDialog.Builder(requireContext())
                 .setTitle("Вы действительно хотите удалить выбранные элементы?")
                 .setNegativeButton("Отменить") { dialog, _ ->
                     dialog.dismiss()
-                }.setPositiveButton("Удалить") { dialog, _ ->
+                }
+                .setPositiveButton("Удалить") { dialog, _ ->
                     selectedItems.forEach { id ->
                         viewModel.delete(id)
                     }
-                    Toast.makeText(requireContext(), "$selectedItems Deleted", Toast.LENGTH_SHORT)
-                        .show()
+                    Toast.makeText(requireContext(), "$selectedItems Deleted", Toast.LENGTH_SHORT).show()
                     dialog.dismiss()
-                }.create()
-
-            alertDialog.show()
+                }
+                .create()
+                .show()
         }
     }
 
@@ -63,12 +80,81 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(R.layout.f
             galleryPhotoAdapter.selectAllItems()
         }
         binding.ivCirclePlusBtn.setOnClickListener {
-            Toast.makeText(requireContext(), "$id", Toast.LENGTH_SHORT).show()
+            openImageGallery()
+        }
+    }
+
+    private fun openImageGallery() {
+        val permission = Manifest.permission.READ_EXTERNAL_STORAGE
+        val grant = PackageManager.PERMISSION_GRANTED
+        if (ContextCompat.checkSelfPermission(requireContext(), permission) == grant) {
+            // У приложения уже есть разрешение на чтение внешнего хранилища
+            startImageGalleryActivity()
+        } else {
+            // Запросить разрешение на чтение внешнего хранилища
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(permission),
+                REQUEST_READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    private fun startImageGalleryActivity() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply {
+            type = "image/*"
+        }
+        startActivityForResult(intent, REQUEST_IMAGE_GALLERY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImageUri = data.data
+            if (selectedImageUri != null) {
+                val imagePath = getImagePath(selectedImageUri)
+                val imageBytes = getImageBytes(imagePath)
+                viewModel.addGalleryPhoto(imageBytes).observe(viewLifecycleOwner) { photoResponse ->
+                    Toast.makeText(requireContext(), "${photoResponse.photo}", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(requireContext(), "Не удалось получить изображение", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getImagePath(uri: Uri): String {
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = requireActivity().contentResolver.query(uri, projection, null, null, null)
+        cursor?.use {
+            val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            it.moveToFirst()
+            return it.getString(columnIndex)
+        }
+        return ""
+    }
+
+    private fun getImageBytes(imagePath: String): ByteArray {
+        val imageFile = File(imagePath)
+        return imageFile.readBytes()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Разрешение на чтение внешнего хранилища было предоставлено
+                startImageGalleryActivity()
+            } else {
+                Toast.makeText(requireContext(), "Не удалось получить разрешение на чтение внешнего хранилища", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     override fun launchObservers() {
-        viewModel.getStats().observe(viewLifecycleOwner) {
+        viewModel.getStats().observe(viewLifecycleOwner) { it ->
             if (it.banners.isEmpty()) {
                 binding.emptyLayout.visibility = View.VISIBLE
             } else {
--- app/src/main/java/com/example/galleryviacatalog/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/galleryviacatalog/ui/home/HomeViewModel.kt
@@ -8,7 +8,11 @@ import com.example.galleryviacatalog.ui.home.api.ApiService
 import com.example.galleryviacatalog.ui.home.api.RetrofitClient
 import com.example.galleryviacatalog.ui.home.base.BaseViewModel
 import com.example.galleryviacatalog.ui.home.model.GetProfile
+import com.example.galleryviacatalog.ui.home.model.PhotoResponse
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,15 +26,15 @@ class HomeViewModel : BaseViewModel() {
     fun getStats(): LiveData<GetProfile> {
         return getS()
     }
-    fun delete(id:Int): LiveData<Unit> {
+
+    fun delete(id: Int): LiveData<Unit> {
         return deleteById(id)
     }
 
     private fun deleteById(id: Int): MutableLiveData<Unit> {
         val data = MutableLiveData<Unit>()
         viewModelScope.launch {
-            apiService.deletePhoto(id).enqueue(object :
-                Callback<Unit> {
+            apiService.deletePhoto(id).enqueue(object : Callback<Unit> {
                 override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
                     Log.e("jopa", "onResponse: ${response.body()}")
                     data.postValue(response.body())
@@ -47,8 +51,7 @@ class HomeViewModel : BaseViewModel() {
     private fun getS(): MutableLiveData<GetProfile> {
         val data = MutableLiveData<GetProfile>()
         viewModelScope.launch {
-            apiService.getProf().enqueue(object :
-                Callback<GetProfile> {
+            apiService.getProf().enqueue(object : Callback<GetProfile> {
                 override fun onResponse(call: Call<GetProfile>, response: Response<GetProfile>) {
                     Log.e("jopa", "onResponse: ${response.body()}")
                     data.postValue(response.body())
@@ -62,4 +65,24 @@ class HomeViewModel : BaseViewModel() {
         return data
     }
 
+    fun addGalleryPhoto(editBanner: ByteArray): MutableLiveData<PhotoResponse> {
+        val data = MutableLiveData<PhotoResponse>()
+        val body = editBanner.toRequestBody("image/png".toMediaTypeOrNull(), 0, editBanner.size)
+        val photoBody = body.let { MultipartBody.Part.createFormData("photo", "photo.png", it) }
+        viewModelScope.launch {
+            apiService.addGalleryPhoto(photoBody).enqueue(object : Callback<PhotoResponse> {
+                override fun onResponse(
+                    call: Call<PhotoResponse>, response: Response<PhotoResponse>
+                ) {
+                    Log.e("jopa", "onResponse: ${response.body()}")
+                    data.postValue(response.body())
+                }
+
+                override fun onFailure(call: Call<PhotoResponse>, t: Throwable) {
+                    Log.wtf("alo", t.localizedMessage)
+                }
+            })
+        }
+        return data
+    }
 }
